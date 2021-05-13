da44f148c331c9de754721ac8d145b4535b27b01
Livia-Lemes <80851755+Livia-Lemes@users.noreply.github.com>
2021-05-13 17:19:52 -0300

    adicionando tudo

---
+++ script.js
@@ -0,0 +1,8 @@
+const anoAtual = 2021
+
+let nome = prompt('Digite o seu nome')
+
+let anoNascimento = prompt('Digite o ano em que você nasceu')
+
+ alert(` Olá, o seu nome é ${nome} e você tem ${anoAtual-anoNascimento} anos`)
+
